6dfb99fa38f33e297d4418057509fbfa1de4c49f
Eduardo Costa <m4c0@users.noreply.github.com>
2023-10-16 12:19:01 +0100

    more asserts

--- poc.cpp
+++ poc.cpp
@@ -18,8 +18,13 @@ struct deleter {
   void operator()(AVFormatContext *c) { avformat_close_input(&c); }
 };
 
+inline void assert(bool cond, const char *msg) {
+  silog::assert(cond, msg);
+  if (!cond)
+    throw 0;
+}
 inline int assert_p(int i, const char *msg) {
-  silog::assert(i >= 0, msg);
+  assert(i >= 0, msg);
   return i;
 }
 
@@ -40,16 +45,12 @@ void run(const char *filename) {
       av_find_best_stream(*fmt_ctx, AVMEDIA_TYPE_VIDEO, -1, -1, nullptr, 0),
       "Could not find video stream");
   auto vst = (*fmt_ctx)->streams[vidx];
+  assert(vst, "Missing video stream");
   auto vdec = avcodec_find_decoder(vst->codecpar->codec_id);
-  if (!vdec) {
-    silog::log(silog::error, "Could not find video codec");
-    throw 1;
-  }
+  assert(vdec, "Could not find video codec");
+
   hai::holder<AVCodecContext, deleter> vdec_ctx{avcodec_alloc_context3(vdec)};
-  if (!*vdec_ctx) {
-    silog::log(silog::error, "Could not allocate video codec context");
-    throw 1;
-  }
+  assert(*vdec_ctx, "Could not allocate video codec context");
   assert_p(avcodec_parameters_to_context(*vdec_ctx, vst->codecpar),
            "Could not copy video codec parameters");
   assert_p(avcodec_open2(*vdec_ctx, vdec, nullptr),
@@ -59,29 +60,16 @@ void run(const char *filename) {
       av_find_best_stream(*fmt_ctx, AVMEDIA_TYPE_AUDIO, -1, -1, nullptr, 0);
   assert_p(aidx, "Could not find video stream");
   auto ast = (*fmt_ctx)->streams[aidx];
+  assert(ast, "Missing audio stream");
   auto adec = avcodec_find_decoder(ast->codecpar->codec_id);
-  if (!adec) {
-    silog::log(silog::error, "Could not find video codec");
-    throw 1;
-  }
+  assert(adec, "Could not find audio codec");
+
   hai::holder<AVCodecContext, deleter> adec_ctx{avcodec_alloc_context3(adec)};
-  if (!*adec_ctx) {
-    silog::log(silog::error, "Could not allocate video codec context");
-    throw 1;
-  }
+  assert(*adec_ctx, "Could not allocate video codec context");
   assert_p(avcodec_parameters_to_context(*adec_ctx, ast->codecpar),
            "Could not copy video codec parameters");
   assert_p(avcodec_open2(*adec_ctx, adec, nullptr),
            "Could not open video codec");
-
-  if (!ast) {
-    silog::log(silog::error, "Missing audio stream");
-    throw 1;
-  }
-  if (!vst) {
-    silog::log(silog::error, "Missing video stream");
-    throw 1;
-  }
 
   silog::log(silog::info, "yay V[%s] A[%s]", vdec->name, adec->name);
 }
