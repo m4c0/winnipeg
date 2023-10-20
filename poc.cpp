aa3ce6e0ec86f2b547877d4b0292488cc0fb5e9e
Eduardo Costa <m4c0@users.noreply.github.com>
2023-10-20 11:40:18 +0100

    poc: todo + random thoughts

--- poc.cpp
+++ poc.cpp
@@ -1,2 +1,14 @@
 #pragma leco app
 import renderer;
+
+// TODO: audio
+// TODO: seek
+// TODO: scripting
+// TODO: better naming
+//
+// Example script (so far):
+// skip 30s
+// co_await 2m (play 2m?)
+// skip 10s
+// co_await 10m
+// ...
